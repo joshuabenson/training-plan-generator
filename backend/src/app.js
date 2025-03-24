2e6a3d4a9a74e7c0cb1262681d1306285ea53cf3
J Benson <bensonjosha@gmail.com>
2025-03-23 23:46:20 -0500

    run-4 just comments

--- backend/src/app.js
+++ backend/src/app.js
@@ -40,6 +40,32 @@ app.post('/generate-plan', async (req, res) => {
         
         if (Array.isArray(weeks)) {
             weeks.forEach(week => {
+
+
+                // we need the schedule to align with the passed in preffered days
+                // for the first verison of this functionality we will just make sure each preferred day has a workout
+                // and once we run out of workouts we can leave the rest as rest days
+                // late I will add in combining of non long runs so that the weekly mileage stays the same even
+                // if only two days of the week are preferred
+
+                // iterate through the ordered preferred days array (ex: ["Monday", "Wednesday", "Friday"]) 
+                // and for each day iterate through the week template and find the first non-rest day
+                // and add it to the processedWeek for that day while removing it from the template so it isn't reused
+                // then fill in the rest of the days with the remaining workouts
+
+                // then for version two, after this process is done we will check to see what workouts are left
+                // and then we will pick the best option to fill in the remaining workout
+                // to do this, we will write a smart algorithm that intelligently finds the ideal day to fit in each remaining workout
+                // this is the algorithm which is based on training principals of intensity and recovery
+                   // each workout day of ther week is assigned an intensity score 
+                   // each day of the week is assigned a recovery score which starts at 0
+                   // the intensity score of a workout adds a recovery score to the *following day of the week*
+                   // and it also maps a recovery score of 1/2 to the day of the week two days later
+                   // Once each recovery score is calculated we can iterate through each possibility where we add the remaining workouts
+                   // to each week to find the combination 
+
+                // console.log('preferredDays',preferredDays);
+                
                 const processedWeek = {};
                 
                 // For each day in the week's schedule
