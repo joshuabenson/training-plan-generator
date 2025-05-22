5ba4c555da97fcb6d9945216933e94b5ffbcdc98
J Benson <bensonjosha@gmail.com>
2025-05-22 00:32:43 -0500

    run-99

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
 
   const handlePlanGeneration = async (formData) => {
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/generate-plan`, {
+      const response = await fetch(`${process.env.RENDER_PUBLIC_API_URL}/generate-plan`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
